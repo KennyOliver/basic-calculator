b66334f38c9973b3d6da745a4e73ba09bdeca2ab
Kenny O <kenny_oliver@icloud.com>
2021-02-05 17:35:39 +0000

    Updated UI

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def div(number1,number2):
   return result
 #====================
 def menu():
-  run = 'True'
-  while run == 'True':
+  run = 'y'
+  while run in ["y","yes"]:
     number1 = int(input("Enter 1st value\n--> "))
     number2 = int(input("Enter 2nd value\n--> "))
     print("Choose the action you would like to perform")
@@ -30,7 +30,11 @@ def menu():
       print(mul(number1,number2))
     elif option == 4:
       print(div(number1,number2))
-    run = input("Enter True or False\n--> ")
+    
+    print("Would you like to continue using Basic Calculator?")
+    print("[Y] Yes\n[N] No")
+    run = input("\n--> ").lower()
 
 # main program
+print("Welcome to Basic Calculator!")
 menu()
